7d1c6646f52c4de2ffa8934147dedc1c7dc6b89f
Felipe Castro <146021799+castroofelipee@users.noreply.github.com>
2023-09-25 19:27:27 -0300

    config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ import os
 class Config:
    
     DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_USERNAME = os.getenv("DB_USERNAME", "seu_usuario_mysql")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "sua_senha_mysql")
-    DB_NAME = os.getenv("DB_NAME", "nome_do_banco")
+    DB_USERNAME = os.getenv("DB_USERNAME", "")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+    DB_NAME = os.getenv("DB_NAME", "")
 
     
     DEBUG = os.getenv("DEBUG", True)
-    SECRET_KEY = os.getenv("SECRET_KEY", "sua_chave_secreta")
+    SECRET_KEY = os.getenv("SECRET_KEY", "")
 
     
     SSL_CA_PATH = os.getenv("SSL_CA_PATH", "/etc/ssl/cert.pem")
